f747fa8d20e48ad3d6f582bad85ddfae0be3c8a2
isaacs <i@izs.me>
2012-07-25 20:07:07 -0700

    code

---
+++ readable.js
@@ -0,0 +1,98 @@
+module.exports = Readable;
+
+var Stream = require('stream');
+var util = require('util');
+
+util.inherits(Readable, Stream);
+
+function Readable(stream) {
+  if (stream) this.wrap(stream);
+  Stream.apply(this);
+}
+
+// override this method.
+Readable.prototype.read = function(n) {
+  return null;
+};
+
+Readable.prototype.pipe = function(dest) {
+  this.on('end', dest.end.bind(dest));
+  flow.call(this);
+
+  function flow() {
+    var chunk;
+    while (chunk = this.read()) {
+      var written = dest.write(chunk);
+      if (!written) {
+        dest.once('drain', flow.bind(this));
+        return;
+      }
+    }
+    this.once('readable', flow);
+  }
+};
+
+// wrap a 'data'/pause()/resume() style stream
+// This is *not* part of the readable stream interface.
+// It is an ugly unfortunate mess of history.
+Readable.prototype.wrap = function(stream) {
+  this._buffer = [];
+  this._bufferLength = 0;
+  var paused = false;
+  stream.on('data', function(chunk) {
+    this._buffer.push(chunk);
+    this._bufferLength += chunk.length;
+    this.emit('readable');
+    // if not consumed, then pause the stream.
+    if (this._bufferLength > 0 && !paused) {
+      paused = true;
+      this._stream.pause();
+    }
+  }.bind(this));
+
+  // consume some bytes.  if not all is consumed, then
+  // pause the underlying stream.
+  this.read = function(n) {
+    var ret;
+
+    if (this._bufferLength === 0) {
+      ret = null;
+    } else if (!n || n >= this._bufferLength) {
+      // read it all
+      ret = Buffer.concat(this._buffer);
+      this._bufferLength = 0;
+    } else {
+      // read just some of it.
+      if (n < this._buffer[0].length) {
+        // just take a part of the first buffer.
+        var buf = this._buffer[0];
+        ret = buf.slice(0, n);
+        this._buffer[0] = buf.slice(n);
+      } else if (n === this._buffer[0].length) {
+        // first buffer is a perfect match
+        ret = this._buffer.shift();
+      } else {
+        // complex case.
+        ret = new Buffer(n);
+        var c = 0;
+        for (var i = 0; i < this._buffer.length && c < n; i++) {
+          var buf = this._buffer[i];
+          var cpy = Math.min(n - c, buf.length);
+          buf.copy(ret, c, 0, cpy);
+          if (cpy < buf.length) {
+            this._buffer[i] = buf.slice(cpy);
+            this._buffer = this._buffer.slice(i);
+          }
+          n -= cpy;
+        }
+      }
+      this._bufferLength -= n;
+    }
+
+    if (this._bufferLength === 0 && paused) {
+      this._stream.resume();
+      paused = false;
+    }
+    return ret;
+  };
+};
