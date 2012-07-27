843d37045f9e2adb1e454f119543cb01723383b1
isaacs <i@izs.me>
2012-07-26 23:03:05 -0700

    Abstract out the 'read n bytes from list' function

---
+++ test/from-list.js
@@ -0,0 +1,32 @@
+var test = require('tap').test;
+var fromList = require('../from-list.js');
+
+test('with length', function(t) {
+  // have a length
+  var len = 16;
+  var list = [ new Buffer('foog'),
+               new Buffer('bark'),
+               new Buffer('bazy'),
+               new Buffer('kuel') ];
+
+  // read more than the first element.
+  var ret = fromList(6, list, 16);
+  t.equal(ret.toString(), 'foogba');
+
+  // read exactly the first element.
+  ret = fromList(2, list, 10);
+  t.equal(ret.toString(), 'rk');
+
+  // read less than the first element.
+  ret = fromList(2, list, 8);
+  t.equal(ret.toString(), 'ba');
+
+  // read more than we have.
+  ret = fromList(100, list, 6);
+  t.equal(ret.toString(), 'zykuel');
+
+  // all consumed.
+  t.same(list, []);
+
+  t.end();
+});
