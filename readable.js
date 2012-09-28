8595376912748ca7984df9047a4a7498fc61f720
isaacs <i@izs.me>
2012-09-27 17:38:16 -0700

    Fix all the things.
    1. Fix #16.  Pipe emits 'data', and switching into old-mode while piping
    is not allowed.
    
    2. Move all the magic fields to a '_readableOptions' member, so that they're
    less likely to be collided with in extension classes.
    
    3. Fix #18.  That was just a normal bug anyway, not any kind of serious
    API issue.
    
    4. Fix #17.  Switch into old-mode on pause()/resume()
    
    5. Fix #8.  If you do read(10) and only 9 bytes are available, and the stream
    is not ending, then it returns null, not 9 bytes.  It'll never return fewer
    bytes than asked for.  If you don't specify a number, then it still gives
    you the full buffer.
    
    This is not a very atomic commit.  But then again, this whole repo is a wip.
    So, deal with it.

--- readable.js
+++ readable.js
@@ -8,45 +8,67 @@ var fromList = require('./from-list.js');
 
 util.inherits(Readable, Stream);
 
-function Readable(options) {
-  options = options || {};
+function ReadableOptions(options, stream) {
+  options = util._extend({}, options);
   this.bufferSize = options.bufferSize || 16 * 1024;
   this.lowWaterMark = options.lowWaterMark || 1024;
   this.buffer = [];
   this.length = 0;
-  this._pipes = [];
-  this._flowing = false;
+  this.pipes = [];
+  this.flowing = false;
+  this.ended = false;
+  this.bufferSize = 0;
+  this.stream = stream;
+}
+
+function Readable(options) {
+  this._readableOptions = new ReadableOptions(options, this);
   Stream.apply(this);
 }
 
 // you can override either this method, or _read(n, cb) below.
 Readable.prototype.read = function(n) {
-  if (this.length === 0 && this.ended) {
+  var opt = this._readableOptions;
+
+  if (opt.length === 0 && opt.ended) {
     process.nextTick(this.emit.bind(this, 'end'));
     return null;
   }
 
-  if (isNaN(n) || n <= 0) n = this.length;
-  n = Math.min(n, this.length);
+  if (isNaN(n) || n <= 0)
+    n = opt.bufferSize || opt.length;
 
-  var ret = n > 0 ? fromList(n, this.buffer, this.length) : null;
-  this.length -= n;
+  // XXX: controversial.
+  // don't have that much.  return null, unless we've ended.
+  if (n > opt.length) {
+    if (!opt.ended)
+      return null;
+    else
+      n = opt.length;
+  }
 
-  if (!this.ended && this.length < this.lowWaterMark) {
+  var ret = n > 0 ? fromList(n, opt.buffer, opt.length) : null;
+  opt.length -= n;
+
+  if (!opt.ended && opt.length < opt.lowWaterMark) {
+    // call internal read method
     this._read(this.bufferSize, function onread(er, chunk) {
-      if (er) return this.emit('error', er);
+      if (er)
+        return this.emit('error', er);
 
       if (!chunk || !chunk.length) {
-        this.ended = true;
-        if (this.length === 0) this.emit('end');
+        opt.ended = true;
+        if (opt.length === 0)
+          this.emit('end');
         return;
       }
 
-      this.length += chunk.length;
-      this.buffer.push(chunk);
-      if (this.length < this.lowWaterMark) {
-        this._read(this.bufferSize, onread.bind(this));
+      opt.length += chunk.length;
+      opt.buffer.push(chunk);
+      if (opt.length < opt.lowWaterMark) {
+        this._read(opt.bufferSize, onread.bind(this));
       }
+      // now we have something to call this.read() to get.
       this.emit('readable');
     }.bind(this));
   }
@@ -55,71 +77,103 @@ Readable.prototype.read = function(n) {
 };
 
 // abstract method.  to be overridden in specific implementation classes.
+// call cb(er, data) where data is <= n in length.
+// for virtual (non-string, non-buffer) streams, "length" is somewhat
+// arbitrary, and perhaps not very meaningful.
 Readable.prototype._read = function(n, cb) {
   process.nextTick(cb.bind(this, new Error('not implemented')));
 };
 
-Readable.prototype.pipe = function(dest, opt) {
+Readable.prototype.pipe = function(dest, pipeOpts) {
   var src = this;
-  src._pipes.push(dest);
-  if ((!opt || opt.end !== false) &&
+  var opt = this._readableOptions;
+  opt.pipes.push(dest);
+
+  if ((!pipeOpts || pipeOpts.end !== false) &&
       dest !== process.stdout && 
       dest !== process.stderr) {
     src.once('end', onend);
     dest.on('unpipe', function(readable) {
-      if (readable === src) {
+      if (readable === src)
         src.removeListener('end', onend);
-      }
     });
   }
-
-  dest.emit('pipe', src);
-  if (!src._flowing) process.nextTick(flow.bind(src));
-  return dest;
 
   function onend() {
     dest.end();
   }
+
+  dest.emit('pipe', src);
+
+  // start the flow.
+  if (!opt.flowing)
+    process.nextTick(flow.bind(src));
+
+  return dest;
 };
 
 function flow(src) {
-  if (!src) src = this;
+  if (!src)
+    src = this;
+
+  var opt = src._readableOptions;
   var chunk;
   var dest;
   var needDrain = 0;
-  while (null !== (chunk = src.read())) {
-    src._pipes.forEach(function(dest, i, list) {
+
+  function ondrain() {
+    needDrain--;
+    if (needDrain === 0)
+      flow(src);
+  }
+
+  while (opt.pipes.length &&
+         null !== (chunk = src.read())) {
+    opt.pipes.forEach(function(dest, i, list) {
       var written = dest.write(chunk);
       if (false === written) {
         needDrain++;
         dest.once('drain', ondrain);
       }
     });
-    if (needDrain > 0) return;
+    src.emit('data', chunk);
+
+    // if anyone needs a drain, then we have to wait for that.
+    if (needDrain > 0)
+      return;
   }
 
+  // if every destination was unpiped, either before entering this
+  // function, or in the while loop, then stop flowing.
+  //
+  // NB: This is a pretty rare edge case.
+  if (opt.pipes.length === 0) {
+    opt.flowing = false;
+
+    // if there were data event listeners added, then switch to old mode.
+    if (this.listeners('data').length)
+      emitDataEvents(this);
+    return;
+  }
+
+  // at this point, no one needed a drain, so we just ran out of data
+  // on the next readable event, start it over again.
   src.once('readable', flow);
-
-  function ondrain() {
-    needDrain--;
-    if (needDrain === 0) {
-      flow(src);
-    }
-  }
 }
 
 Readable.prototype.unpipe = function(dest) {
+  var opt = this._readableOptions;
   if (!dest) {
     // remove all of them.
-    this._pipes.forEach(function(dest, i, list) {
+    opt.pipes.forEach(function(dest, i, list) {
       dest.emit('unpipe', this);
     }, this);
-    this._pipes.length = 0;
+    opt.pipes.length = 0;
   } else {
-    var i = this._pipes.indexOf(dest);
+    var i = opt.pipes.indexOf(dest);
     if (i !== -1) {
       dest.emit('unpipe', this);
-      this._pipes.splice(i, 1);
+      opt.pipes.splice(i, 1);
     }
   }
   return this;
@@ -129,12 +183,35 @@ Readable.prototype.unpipe = function(dest) {
 // This is *not* part of the new readable stream interface.
 // It is an ugly unfortunate mess of history.
 Readable.prototype.on = function(ev, fn) {
-  if (ev === 'data') emitDataEvents(this);
+  // https://github.com/isaacs/readable-stream/issues/16
+  // if we're already flowing, then no need to set up data events.
+  if (ev === 'data' && !this._readableOptions.flowing)
+    emitDataEvents(this);
+
   return Stream.prototype.on.call(this, ev, fn);
 };
 Readable.prototype.addListener = Readable.prototype.on;
 
+// pause() and resume() are remnants of the legacy readable stream API
+// If the user uses them, then switch into old mode.
+Readable.prototype.resume = function() {
+  emitDataEvents(this);
+  return this.resume();
+};
+
+Readable.prototype.pause = function() {
+  emitDataEvents(this);
+  return this.pause();
+};
+
 function emitDataEvents(stream) {
+  var opt = stream._readableOptions;
+
+  if (opt.flowing) {
+    // https://github.com/isaacs/readable-stream/issues/16
+    throw new Error('Cannot switch to old mode now.');
+  }
+
   var paused = false;
   var readable = false;
 
@@ -149,7 +226,8 @@ function emitDataEvents(stream) {
     while (!paused && (c = stream.read())) {
       stream.emit('data', c);
     }
-    if (c === null) readable = false;
+    if (c === null)
+      readable = false;
   });
 
   stream.pause = function() {
@@ -158,32 +236,31 @@ function emitDataEvents(stream) {
 
   stream.resume = function() {
     paused = false;
-    if (readable) stream.emit('readable');
+    if (readable)
+      stream.emit('readable');
   };
 }
 
-// wrap an old-style stream
+// wrap an old-style stream as the async data source.
 // This is *not* part of the readable stream interface.
 // It is an ugly unfortunate mess of history.
 Readable.prototype.wrap = function(stream) {
-  this.buffer = [];
-  this.length = 0;
+  var opt = this._readableOptions;
   var paused = false;
-  var ended = false;
 
   stream.on('end', function() {
-    ended = true;
-    if (this.length === 0) {
+    opt.ended = true;
+    if (opt.length === 0)
       this.emit('end');
-    }
   }.bind(this));
 
   stream.on('data', function(chunk) {
-    this.buffer.push(chunk);
-    this.length += chunk.length;
+    opt.buffer.push(chunk);
+    opt.length += chunk.length;
     this.emit('readable');
+
     // if not consumed, then pause the stream.
-    if (this.length > this.lowWaterMark && !paused) {
+    if (opt.length > opt.lowWaterMark && !paused) {
       paused = true;
       stream.pause();
     }
@@ -209,21 +286,30 @@ Readable.prototype.wrap = function(stream) {
   // consume some bytes.  if not all is consumed, then
   // pause the underlying stream.
   this.read = function(n) {
-    if (this.length === 0) return null;
+    if (opt.length === 0)
+      return null;
 
-    if (isNaN(n) || n <= 0) n = this.length;
+    if (isNaN(n) || n <= 0)
+      n = opt.bufferSize || opt.length;
 
-    var ret = fromList(n, this.buffer, this.length);
-    this.length = Math.max(0, this.length - n);
+    if (n > opt.length) {
+      if (!opt.ended)
+        return null;
+      else
+        n = opt.length;
+    }
 
-    if (this.length < this.lowWaterMark && paused) {
+    var ret = fromList(n, opt.buffer, opt.length);
+    opt.length -= n;
+
+    if (opt.length < opt.lowWaterMark && paused) {
       stream.resume();
       paused = false;
     }
 
-    if (this.length === 0 && ended) {
+    if (opt.length === 0 && ended)
       process.nextTick(this.emit.bind(this, 'end'));
-    }
+
     return ret;
   };
 };
