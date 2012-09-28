a582d542688373df96864df9976c8f424a8e9037
isaacs <i@izs.me>
2012-09-28 16:46:27 -0700

    Use the newer patterns on the fs lib

--- fs.js
+++ fs.js
@@ -34,24 +34,20 @@ function allocNewPool() {
 util.inherits(FSReadable, Readable);
 
 function FSReadable(path, options) {
-  if (!options) options = {};
+  if (!options)
+    options = {};
 
-  Readable.apply(this, options);
+  Readable.call(this, options);
 
+  var state = this._readableState;
   this.path = path;
-  this.flags = 'r';
-  this.mode = 438; //=0666
-  this.fd = null;
-  this.bufferSize = 64 * 1024;
-  this.lowWaterMark = 16 * 1024;
+  this.flags = options.flags || 'r';
+  this.mode = options.mode || 438; //=0666
+  this.fd = options.fd || null;
 
-  Object.keys(options).forEach(function(k) {
-    this[k] = options[k];
-  }, this);
-
-  // cast to an int
-  assert(typeof this.bufferSize === 'number');
-  this.bufferSize = ~~this.bufferSize;
+  // a little bit bigger buffer and watermark by default
+  state.bufferSize = options.bufferSize || 64 * 1024;
+  state.lowWaterMark = options.lowWaterMark || 16 * 1024;
 
   if (this.encoding) {
     this._decoder = new StringDecoder(this.encoding);
@@ -59,22 +55,20 @@ function FSReadable(path, options) {
 
   var typeofStart = typeof this.start;
   if (typeofStart !== 'undefined') {
-    if (typeofStart !== 'number') {
+    if (typeofStart !== 'number')
       throw new TypeError('start must be a Number');
-    }
+
     var typeofEnd = typeof this.end;
-    if (typeofEnd === 'undefined') {
+    if (typeofEnd === 'undefined')
       this.end = Infinity;
-    } else if (typeofEnd !== 'number') {
+    else if (typeofEnd !== 'number')
       throw new TypeError('end must be a Number');
-    }
 
     this.pos = this.start;
   }
 
-  if (typeof this.fd !== 'number') {
+  if (typeof this.fd !== 'number')
     this.open();
-  }
 }
 
 FSReadable.prototype.open = function() {
@@ -88,7 +82,7 @@ FSReadable.prototype.open = function() {
     this.fd = fd;
     this.emit('open', fd);
   }.bind(this));
-}
+};
 
 FSReadable.prototype._read = function(n, cb) {
   if (this.fd === null) {
@@ -96,8 +90,8 @@ FSReadable.prototype._read = function(n, cb) {
     return;
   }
 
-  if (this.reading || this.ended || this.destroyed) return;
-  this.reading = true;
+  if (this.destroyed)
+    return;
 
   if (!pool || pool.length - pool.used < minPoolSpace) {
     // discard the old pool. Can't add to the free list because
@@ -110,12 +104,11 @@ FSReadable.prototype._read = function(n, cb) {
   var toRead = Math.min(pool.length - pool.used, n);
   var start = pool.used;
 
-  if (this.pos !== undefined) {
+  if (this.pos !== undefined)
     toRead = Math.min(this.end - this.pos + 1, toRead);
-  }
+
 
   if (toRead <= 0) {
-    this.reading = false;
     this.emit('readable');
     return;
   }
@@ -123,32 +116,34 @@ FSReadable.prototype._read = function(n, cb) {
   fs.read(this.fd, pool, pool.used, toRead, this.pos, onread.bind(this));
 
   function onread(er, bytesRead) {
-    this.reading = false;
-
     if (er) {
       this.destroy();
       return cb(er);
     }
 
     var b = null;
-    if (bytesRead > 0) {
+    if (bytesRead > 0)
       b = thisPool.slice(start, start + bytesRead);
-    }
+
     cb(null, b);
   }
-}
+};
 
 FSReadable.prototype.close = function(cb) {
-  if (cb) this.once('close', cb);
+  if (cb)
+    this.once('close', cb);
   if (this.closed || this.fd === null) {
-    if (this.fd === null) this.once('open', this.destroy);
+    if (this.fd === null)
+      this.once('open', this.destroy);
     return process.nextTick(this.emit.bind(this, 'close'));
   }
   this.closed = true;
 
   fs.close(this.fd, function(er) {
-    if (er) this.emit('error', er);
-    else this.emit('close');
+    if (er)
+      this.emit('error', er);
+    else
+      this.emit('close');
   }.bind(this));
 };
 
