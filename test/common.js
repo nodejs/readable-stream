c31f5a23a91bec7e4b78b32d81b0204c66a20ebf
Rod Vagg <rod@vagg.org>
2014-03-29 22:19:24 +1100

    better 0.8 support

--- test/common.js
+++ test/common.js
@@ -92,14 +92,14 @@ process.on('exit', function() {
   if (!exports.globalCheck) return;
   var knownGlobals = [setTimeout,
                       setInterval,
-                      typeof setImmediate == 'undefined' ? null : setImmediate,
+                      setImmediate,
                       clearTimeout,
                       clearInterval,
-                      typeof clearImmediate == 'undefined' ? null : clearImmediate,
+                      clearImmediate,
                       console,
                       Buffer,
                       process,
-                      global].filter(Boolean);
+                      global];
 
   if (global.gc) {
     knownGlobals.push(gc);
@@ -139,7 +139,8 @@ process.on('exit', function() {
   }
 
   /*<replacement>*/
-  if (typeof constructor == 'function') knownGlobals.push(constructor);
+  if (typeof constructor == 'function')
+    knownGlobals.push(constructor);
   /*</replacement>*/
 
   for (var x in global) {
@@ -221,3 +222,15 @@ if (!util._errnoException) {
     return e;
   };
 }
+/*<replacement>*/
+if (!global.setImmediate) {
+  global.setImmediate = function setImmediate(fn) {
+    return setTimeout(fn, 0);
+  };
+}
+if (!global.clearImmediate) {
+  global.clearImmediate = function clearImmediate(i) {
+  return clearTimeout(i);
+  };
+}
+/*</replacement>*/
