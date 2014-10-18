e6996f1d8c71994da4b8f08fd6ba3f31b4567661
Sam Newman <newmansam@outlook.com>
2014-10-18 01:34:20 +0100

    cached stream reference
    Conflicts:
    	readable.js

--- readable.js
+++ readable.js
@@ -1,5 +1,6 @@
-require('stream'); // hack to fix a circular dependency issue when used with browserify
+var Stream = require('stream'); // hack to fix a circular dependency issue when used with browserify
 exports = module.exports = require('./lib/_stream_readable.js');
+exports.Stream = Stream;
 exports.Readable = exports;
 exports.Writable = require('./lib/_stream_writable.js');
 exports.Duplex = require('./lib/_stream_duplex.js');
