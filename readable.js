8b1d54c54e9a6450e3e0b252ba38db40a18bd9c0
Calvin Metcalf <cmetcalf@appgeo.com>
2017-03-17 12:52:30 -0400

    2 input files

--- readable.js
+++ readable.js
@@ -1,14 +1,12 @@
-exports = module.exports = require('./lib/_stream_readable.js');
-exports.Stream = exports;
-exports.Readable = exports;
-exports.Writable = require('./lib/_stream_writable.js');
-exports.Duplex = require('./lib/_stream_duplex.js');
-exports.Transform = require('./lib/_stream_transform.js');
-exports.PassThrough = require('./lib/_stream_passthrough.js');
-
-if (!process.browser) {
-  var Stream = require('st' + 'ream');
-  if (process.env.READABLE_STREAM === 'disable' && Stream) {
-    module.exports =  Stream;
-  }
+var Stream = require('stream');
+if (process.env.READABLE_STREAM === 'disable' && Stream) {
+  module.exports = Stream;
+} else {
+  exports = module.exports = require('./lib/_stream_readable.js');
+  exports.Stream = Stream || exports;
+  exports.Readable = exports;
+  exports.Writable = require('./lib/_stream_writable.js');
+  exports.Duplex = require('./lib/_stream_duplex.js');
+  exports.Transform = require('./lib/_stream_transform.js');
+  exports.PassThrough = require('./lib/_stream_passthrough.js');
 }
