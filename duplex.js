f04e446fe224739934de8440678f9b7f6ff8fab1
Matteo Collina <hello@matteocollina.com>
2017-03-28 19:06:39 +0200

    Maintain the same interface when using READABLE_STREAM=disable

--- duplex.js
+++ duplex.js
@@ -1,1 +1,13 @@
-module.exports = require("./lib/_stream_duplex.js")
+var Duplex = require("./lib/_stream_duplex.js")
+
+if (!process.browser && process.env.READABLE_STREAM === 'disable') {
+  var Stream = (function (){
+    try {
+      return require('st' + 'ream'); // hack to fix a circular dependency issue when used with browserify
+    } catch(_){}
+  }());
+
+  module.exports = Stream && Stream.Duplex || Duplex
+}
+
+module.exports = Duplex
--- passthrough.js
+++ passthrough.js
@@ -1,1 +1,13 @@
-module.exports = require("./lib/_stream_passthrough.js")
+var PassThrough = require("./lib/_stream_passthrough.js")
+
+if (!process.browser && process.env.READABLE_STREAM === 'disable') {
+  var Stream = (function (){
+    try {
+      return require('st' + 'ream'); // hack to fix a circular dependency issue when used with browserify
+    } catch(_){}
+  }());
+
+  module.exports = Stream && Stream.PassThrough || PassThrough
+}
+
+module.exports = PassThrough
