e1ce9eabcfbf70b3c9c72549a08dd7784c75b3b9
Matteo Collina <hello@matteocollina.com>
2017-03-29 10:24:50 +0200

    Added -browser.js files for Duplex and Writable.

--- duplex.js
+++ duplex.js
@@ -1,13 +1,1 @@
-var Duplex = require("./lib/_stream_duplex.js")
-
-if (!process.browser && process.env.READABLE_STREAM === 'disable') {
-  var Stream = (function (){
-    try {
-      return require('st' + 'ream'); // hack to fix a circular dependency issue when used with browserify
-    } catch(_){}
-  }());
-
-  module.exports = Stream && Stream.Duplex || Duplex
-}
-
-module.exports = Duplex
+module.exports = require('./readable').Duplex
