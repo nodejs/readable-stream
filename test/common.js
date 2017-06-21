cf1657e6b50686a6ad0590119530576a623c2323
Matteo Collina <hello@matteocollina.com>
2017-06-21 11:17:00 +0200

    Import from Node 8.1.2.

--- test/common.js
+++ test/common.js
@@ -790,6 +790,30 @@ exports.getTTYfd = function getTTYfd() {
   return tty_fd;
 };
 
+// Hijack stdout and stderr
+var stdWrite = {};
+function hijackStdWritable(name, listener) {
+  var stream = process[name];
+  var _write = stdWrite[name] = stream.write;
+
+  stream.writeTimes = 0;
+  stream.write = function (data, callback) {
+    listener(data);
+    _write.call(stream, data, callback);
+    stream.writeTimes++;
+  };
+}
+
+function restoreWritable(name) {
+  process[name].write = stdWrite[name];
+  delete process[name].writeTimes;
+}
+
+exports.hijackStdout = hijackStdWritable.bind(null, 'stdout');
+exports.hijackStderr = hijackStdWritable.bind(null, 'stderr');
+exports.restoreStdout = restoreWritable.bind(null, 'stdout');
+exports.restoreStderr = restoreWritable.bind(null, 'stderr');
+
 function forEach(xs, f) {
   for (var i = 0, l = xs.length; i < l; i++) {
     f(xs[i], i);
