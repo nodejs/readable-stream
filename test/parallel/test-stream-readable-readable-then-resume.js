e002d327a43d10919749636ffe1c1a3b27fdd011
Matteo Collina <hello@matteocollina.com>
2019-02-28 17:42:14 +0100

    Updated to v10.15.2

---
+++ test/parallel/test-stream-readable-readable-then-resume.js
@@ -0,0 +1,49 @@
+"use strict";
+
+/*<replacement>*/
+var bufferShim = require('safe-buffer').Buffer;
+/*</replacement>*/
+
+
+var common = require('../common');
+
+var _require = require('../../'),
+    Readable = _require.Readable; // This test verifies that a stream could be resumed after
+// removing the readable event in the same tick
+
+
+check(new Readable({
+  objectMode: true,
+  highWaterMark: 1,
+  read: function read() {
+    if (!this.first) {
+      this.push('hello');
+      this.first = true;
+      return;
+    }
+
+    this.push(null);
+  }
+}));
+
+function check(s) {
+  var readableListener = common.mustNotCall();
+  s.on('readable', readableListener);
+  s.on('end', common.mustCall());
+  s.removeListener('readable', readableListener);
+  s.resume();
+}
+
+;
+
+require('tap').pass('sync run');
+
+var _list = process.listeners('uncaughtException');
+
+process.removeAllListeners('uncaughtException');
+
+_list.pop();
+
+_list.forEach(function (e) {
+  return process.on('uncaughtException', e);
+});
