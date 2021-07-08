a10d96e4b8c55fc6152457a7c64a29e4e7c58fee
ptaylor <paul.e.taylor@me.com>
2021-07-08 02:36:32 -0500

    add semi

--- readable.js
+++ readable.js
@@ -3,7 +3,7 @@ if (process.env.READABLE_STREAM === 'disable' && Stream) {
   module.exports = Stream.Readable;
   Object.entries(Object.getOwnPropertyDescriptors(Stream))
     .filter((entry) => entry[1].writable && entry[0] !== 'prototype')
-    .forEach((entry) => module.exports[entry[0]] = Stream[entry[0]])
+    .forEach((entry) => module.exports[entry[0]] = Stream[entry[0]]);
   module.exports.Stream = Stream;
 } else {
   exports = module.exports = require('./lib/_stream_readable.js');
