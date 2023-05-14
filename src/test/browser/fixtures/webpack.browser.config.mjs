f3126b66ad7fde8b6b55a43bd4ea40fee28edc66
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2023-05-14 17:59:16 +0200

    chore: remove unecessary banner plugin

--- src/test/browser/fixtures/webpack.browser.config.mjs
+++ src/test/browser/fixtures/webpack.browser.config.mjs
@@ -1,9 +1,6 @@
-import { createRequire } from 'module'
 import { resolve } from 'path'
 import { fileURLToPath } from 'url'
-import webpack from 'webpack'
 
-const require = createRequire(import.meta.url)
 const rootDir = resolve(fileURLToPath(new URL('.', import.meta.url)), '../../../')
 
 export default {
@@ -15,12 +12,6 @@ export default {
   mode: 'production',
   target: 'web',
   performance: false,
-  plugins: [
-    new webpack.BannerPlugin({
-      banner: 'function setImmediate(fn, ...args) { setTimeout(() => fn(...args), 1) }',
-      raw: true
-    }),
-  ],
   resolve: {
     aliasFields: ['browser'],
   }
