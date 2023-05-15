5f6966dc974bf13d859324fcafac33ec8464bd9c
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2023-05-15 21:58:07 +0200

    chore: fix webpacking dist

--- readable-stream-test/webpack.config.mjs
+++ readable-stream-test/webpack.config.mjs
@@ -1,13 +1,13 @@
 import { resolve } from 'path'
 import { fileURLToPath } from 'url'
 
-const rootDir = resolve(fileURLToPath(new URL('.', import.meta.url)), '../../../')
+const rootDir = resolve(fileURLToPath(new URL('.', import.meta.url)), '../')
 
 export default {
   mode: 'development',
   entry: './readable-stream-test/import.js',
   output: {
-    path: resolve(rootDir, 'bundling', 'dist'),
+    path: resolve(rootDir, 'readable-stream-test', 'dist'),
     filename: 'import.js'
   }
 }
